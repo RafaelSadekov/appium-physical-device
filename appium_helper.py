5988b55da05e5c7ffdc4616bbdfc135aa63be136
Rafael Sadekov <rafael.sadekov@vk.com>
2025-03-04 02:20:06 +0300

    upd helper

--- appium_helper.py
+++ appium_helper.py
@@ -2,12 +2,13 @@ from appium import webdriver
 from appium.options.android import UiAutomator2Options
 import time
 
-def get_driver(app_package=None, app_activity=None):
+def get_driver(app_package=None, app_activity=None, no_reset=False):
     """
     Подключается к Appium и возвращает WebDriver.
-    
-    :param app_package: Указывает пакет приложения, которое нужно запустить (например, "com.android.vending").
-    :param app_activity: Указывает стартовую активность приложения (например, ".AssetBrowserActivity").
+
+    :param app_package: Указывает пакет приложения (например, "com.twinby.app").
+    :param app_activity: Указывает стартовую активность приложения.
+    :param no_reset: Если True, не сбрасывает данные приложения.
     :return: WebDriver, готовый к работе.
     """
 
@@ -20,6 +21,10 @@ def get_driver(app_package=None, app_activity=None):
     if app_package and app_activity:
         options.app_package = app_package
         options.app_activity = app_activity
+
+    # Добавляем параметр noReset
+    if no_reset:
+        options.no_reset = True
 
     # Подключаемся к Appium
     driver = webdriver.Remote("http://localhost:4723", options=options)
